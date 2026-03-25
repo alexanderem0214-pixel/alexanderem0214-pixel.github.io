5a3c6155473b60c5f841665ebbcc3caa1371e615
res314 <alexanderem0214@gmail.com>
2026-03-25 01:08:01 -0600

    Delete ProblemDetailError.js

--- ProblemDetailError.js
+++
@@ -1,7 +0,0 @@
-export class ProblemDetailsError extends Error {
-  constructor(problemDetails) {
-    super(problemDetails.detail || "Error en la solicitud");
-    this.problemDetails = problemDetails;
-    this.name = "ProblemDetailsError";
-  }
-}
